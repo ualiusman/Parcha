ac94e4b2fac27e19d71036da8f0959b7d2699451
Usman Ali <usmanaliua.uali@gmail.com>
2018-12-28 23:43:12 +0500

    Form Validation on Quiz

--- Parcha/Parcha/ClientApp/src/app/app.module.ts
+++ Parcha/Parcha/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -54,6 +54,7 @@ import { PageNotFoundComponent } from './components/pagenotfound/pagenotfound.co
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'quiz/create', component: QuizEditComponent },
--- Parcha/Parcha/ClientApp/src/app/components/quiz/quiz-edit.component.ts
+++ Parcha/Parcha/ClientApp/src/app/components/quiz/quiz-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
+import { FormGroup, FormControl, FormBuilder, Validators } from
+  '@angular/forms';
 @Component({
   selector: "quiz-edit",
   templateUrl: './quiz-edit.component.html',
@@ -9,15 +11,21 @@ import { HttpClient } from "@angular/common/http";
 export class QuizEditComponent {
   title: string;
   quiz: Quiz;
+  form: FormGroup;
   // this will be TRUE when editing an existing quiz,
   // FALSE when creating a new one.
   editMode: boolean;
   constructor(private activatedRoute: ActivatedRoute,
     private router: Router,
     private http: HttpClient,
+    private fb: FormBuilder,
     @Inject('BASE_URL') private baseUrl: string) {
     // create an empty object from the Quiz interface
     this.quiz = <Quiz>{};
+
+    // initialize the form
+    this.createForm();
+
     var id = +this.activatedRoute.snapshot.params["id"];
     if (id) {
       this.editMode = true;
@@ -26,6 +34,10 @@ export class QuizEditComponent {
       this.http.get<Quiz>(url).subscribe(res => {
         this.quiz = res;
         this.title = "Edit - " + this.quiz.Title;
+
+        // update the form with the quiz value
+        this.updateForm();
+
       }, error => console.error(error));
     }
     else {
@@ -34,10 +46,22 @@ export class QuizEditComponent {
     }
   }
   onSubmit(quiz: Quiz) {
+
+    // build a temporary quiz object from form values
+    var tempQuiz = <Quiz>{};
+    tempQuiz.Title = this.form.value.Title;
+    tempQuiz.Description = this.form.value.Description;
+    tempQuiz.Text = this.form.value.Text;
+
     var url = this.baseUrl + "api/quiz";
     if (this.editMode) {
+
+      // don't forget to set the tempQuiz Id,
+      // otherwise the EDIT would fail!
+      tempQuiz.Id = this.quiz.Id;
+
       this.http
-        .post<Quiz>(url, quiz)
+        .post<Quiz>(url, tempQuiz)
         .subscribe(res => {
           var v = res;
           console.log("Quiz " + v.Id + " has been updated.");
@@ -46,7 +70,7 @@ export class QuizEditComponent {
     }
     else {
       this.http
-        .put<Quiz>(url, quiz)
+        .put<Quiz>(url, tempQuiz)
         .subscribe(res => {
           var q = res;
           console.log("Quiz " + q.Id + " has been created.");
@@ -56,5 +80,41 @@ export class QuizEditComponent {
   }
   onBack() {
     this.router.navigate(["home"]);
+  }
+
+
+  createForm() {
+    this.form = this.fb.group({
+      Title: ['', Validators.required],
+      Description: '',
+      Text: ''
+    });
+  }
+
+  updateForm() {
+    this.form.setValue({
+      Title: this.quiz.Title,
+      Description: this.quiz.Description || '',
+      Text: this.quiz.Text || ''
+    });
+  }
+
+  getFormControl(name: string) {
+    return this.form.get(name);
+  }
+
+  isValid(name: string) {
+    var e = this.getFormControl(name);
+    return e && e.valid;
+  }
+
+  isChanged(name: string) {
+    var e = this.getFormControl(name);
+    return e && (e.dirty || e.touched);
+  }
+
+  hasError(name: string) {
+    var e = this.getFormControl(name);
+    return e && (e.dirty || e.touched) && !e.valid;
   }
 }
